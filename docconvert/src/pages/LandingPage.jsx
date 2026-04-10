da6c6fe084df189e630072cadde0a045d124afd7
charantm7 <charantm8787@gmail.com>
2026-04-10 23:40:20 +0530

    .

--- docconvert/src/pages/LandingPage.jsx
+++ docconvert/src/pages/LandingPage.jsx
@@ -22,7 +22,7 @@ import HeroSection from "../components/landing/HeroSection";
 import FeaturesSection from "../components/landing/FeaturesSection";
 import HowItWorksSection from "../components/landing/HowItWorksSection";
 import PricingSection from "../components/landing/PricingSection";
-
+// 
 
 export default function LandingPage({ navigate }) {
   return (
